ca5c7728de0617bfd54c48bc247587d639f91553
George <97884009+GiorgosPapado@users.noreply.github.com>
2023-11-23 15:22:43 +0200

    pushing to hub final version

---
+++ GestureType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.VRGestures.Core
+{
+    /*
+    public enum VRControllerGestureType
+    {
+        I,
+        U,
+        INFINITY,
+        LessThan,
+        None
+    }*/
+
+    public enum VRControllerGestureType
+    {
+        I,
+        U,
+        INFINITY,
+        LessThan,
+        O,
+        L,
+        e,
+        M,
+        Negative,
+        None
+    }
+    public static class GestureTypeHelpers
+    {
+        public static string ToGestureString(this VRControllerGestureType gesture)
+        {
+            //string[] gestureNames = new string[] { "Inventory", "OK", "INFINITY", "Back", "None" };
+            string[] gestureNames = new string[] { "Inventory", "OK", "INFINITY", "Back", "Lobby", "Lab", "Meeting Room", "Map", "Negative", "None" };
+            return gestureNames[(int)gesture];
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Usage
+
+1. use NPM package `light-server` to serve the current folder at http://localhost:8080/
+   ```sh
+   npm install npx
+   cd project location
+   npx light-server -s . -p 8080
+   ```
+
+2. open your browser and navigate to the URL.
